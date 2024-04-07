282cdd8515192c5971713960c9058f4291874f3e
Teodor Heggelund <git@teod.eu>
2024-04-07 12:56:59 +0200

    TODO and NICE TO HAVE

--- src/teodorlu/scicloj_garden/step1.clj
+++ src/teodorlu/scicloj_garden/step1.clj
@@ -4,3 +4,15 @@
 ;; not get stuck in analysis paralysis
 
 ;; for now, THIS NS is everything, we drive everything from the REPL.
+
+;; TODO
+;;
+;; - Build an index.html for the currmap page
+;; - Build a toplevel index.html
+;;
+;; NICE TO HAVE
+;;
+;; - Make it fast
+;;   - Pure babashka / JVM
+;;   - Avoid rework (when possible)
+;; - Avoid using Pandoc for the IR to HTML step
