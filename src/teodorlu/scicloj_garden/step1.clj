c97322f389071bd5b47a77e131aaae3418243d78
Teodor Heggelund <git@teod.eu>
2024-04-07 12:45:45 +0200

    step 1

---
+++ src/teodorlu/scicloj_garden/step1.clj
@@ -0,0 +1,6 @@
+(ns teodorlu.scicloj-garden.step1)
+
+;; no organization just yet, just try to get started
+;; not get stuck in analysis paralysis
+
+;; for now, THIS NS is everything, we drive everything from the REPL.
