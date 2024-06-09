1ae5b4c5b65f7ce44ac88a69ade8efeeeaf5345f
Teodor Heggelund <git@teod.eu>
2024-06-09 13:34:39 +0200

    Delete dead code

--- src/teodorlu/scicloj_garden/pandoc2hiccup.clj
+++ src/teodorlu/scicloj_garden/pandoc2hiccup.clj
@@ -4,29 +4,6 @@
    pandocir.ir
    clojure.walk))
 
-(defn ^:private pandoc-el->hiccup-old [el]
-  (cond (= "Para" (:t el))
-        (into [:p] (map pandoc-el->hiccup-old (:c el)))
-
-        (= "Str" (:t el))
-        (:c el)
-
-        (= "Space" (:t el))
-        " "
-
-        (= "Emph" (:t el))
-        (into [:em] (map pandoc-el->hiccup-old (:c el)))
-
-        (= "Header" (:t el))
-        (when-let [header-tag (get {1 :h1 2 :h2 3 :h3 4 :h4 5 :h5 6 :h6}
-                              (first (:c el)))]
-          (into [header-tag] (map pandoc-el->hiccup-old (get (:c el) 2))))
-
-        :else nil))
-
-(defn ^:private pandoc->hiccup-old [pandoc]
-  (apply list (map pandoc-el->hiccup-old (:blocks pandoc))))
-
 (defn ^:private strip-hiccup-header-attrs [hiccup]
   (let [hiccup-node? (fn [node]
                        (and (vector? node)
