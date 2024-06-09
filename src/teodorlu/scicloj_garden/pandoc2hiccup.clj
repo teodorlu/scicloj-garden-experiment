b596c33572f24c27e78ac133a9f05ed8acafd29c
Teodor Heggelund <git@teod.eu>
2024-06-09 10:42:19 +0200

    clean up names

--- src/teodorlu/scicloj_garden/pandoc2hiccup.clj
+++ src/teodorlu/scicloj_garden/pandoc2hiccup.clj
@@ -3,9 +3,9 @@
    pandocir.hiccup
    pandocir.ir))
 
-(defn ^:private pandoc-el->hiccup [el]
+(defn ^:private pandoc-el->hiccup-old [el]
   (cond (= "Para" (:t el))
-        (into [:p] (map pandoc-el->hiccup (:c el)))
+        (into [:p] (map pandoc-el->hiccup-old (:c el)))
 
         (= "Str" (:t el))
         (:c el)
@@ -14,31 +14,28 @@
         " "
 
         (= "Emph" (:t el))
-        (into [:em] (map pandoc-el->hiccup (:c el)))
+        (into [:em] (map pandoc-el->hiccup-old (:c el)))
 
         (= "Header" (:t el))
         (let [header-tag (get {1 :h1 2 :h2 3 :h3 4 :h4 5 :h5 6 :h6}
                               (first (:c el)))]
-          (into [header-tag] (map pandoc-el->hiccup (get (:c el) 2))))
+          (into [header-tag] (map pandoc-el->hiccup-old (get (:c el) 2))))
 
         :else nil))
 
-(defn ^:private pandoc->hiccup [pandoc]
-  (apply list (map pandoc-el->hiccup (:blocks pandoc))))
+(defn ^:private pandoc->hiccup-old [pandoc]
+  (apply list (map pandoc-el->hiccup-old (:blocks pandoc))))
 
-(defn ir->hiccup [ir]
+(defn ^:private ir->hiccup [ir]
   (->> ir
        pandocir.hiccup/ir->hiccup
        :blocks
        (apply list)))
 
-(defn pandoc->hiccup-old [pandoc]
-  (pandoc->hiccup pandoc))
-
-(defn pandoc->hiccup-new [pandoc]
+(defn ^:private pandoc->hiccup-new [pandoc]
   (-> pandoc
       pandocir.ir/pandoc->ir
       ir->hiccup))
 
-(defn pandoc->hiccup [pandoc]
+(defn pandoc2hiccup [pandoc]
   (pandoc->hiccup-old pandoc))
